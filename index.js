df9a378042f8b97db50a4d6755cfeca153b28a76
wold21 <wold2180@gmail.com>
2021-12-05 17:16:43 +0900

    join 정리

--- index.js
+++ index.js
@@ -8,14 +8,8 @@ const {
 const dotenv = require("dotenv");
 dotenv.config();
 
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_PRESENCES,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES,
-    ],
-});
+const client = new Client({ intents: [32767] });
+// const prefix = "!";
 
 client.once("ready", () => {
     console.log("Ready!");
@@ -23,65 +17,65 @@ client.once("ready", () => {
 });
 
 let userList = [];
+// client.on("messageCreate", async (message) => {
+//     const args = message.content.slice(prefix.length).trim().split(/ +/g);
+//     const command = args.shift();
+//     if (command === "모집") {
+//         const embed = new MessageEmbed().setColor("#3498DB").setTitle("🛎 내전 모집");
+
+//         const btns = new MessageActionRow().addComponents(
+//             new MessageButton().setCustomId("join").setLabel("참가").setStyle("SUCCESS"),
+//             new MessageButton().setCustomId("not").setLabel("참가안함").setStyle("DANGER")
+//         );
+
+//         message.channel.send({ embeds: [embed], components: [btns] });
+//     }
+// });
 
 client.on("interactionCreate", async (interection) => {
     const { commandName } = interection;
     if (!interection.isCommand()) return;
     if (commandName === "모집") {
         const confirm = new MessageActionRow().addComponents(
-            new MessageButton()
-                .setCustomId("join")
-                .setLabel("참  가")
-                .setStyle("SUCCESS"),
-            new MessageButton()
-                .setCustomId("not join")
-                .setLabel("참가안함")
-                .setStyle("DANGER")
+            new MessageButton().setCustomId("join").setLabel("참가").setStyle("SUCCESS"),
+            new MessageButton().setCustomId("not").setLabel("참가안함").setStyle("DANGER")
         );
         const embed = new MessageEmbed().setColor("#3498DB").setTitle("🛎 내전 모집");
 
         await interection.reply({
             embeds: [embed],
-            ephemeral: true,
             components: [confirm],
         });
 
-        const collector = interection.channel.createMessageComponentCollector({
-            max: 2,
-        });
+        // collector.on("collect", async (i) => {
+        //     if (i.customId === "join") {
+        //         const editBtn = new MessageEmbed()
+        //             .setColor("#3498DB")
+        //             .setDescription("참가를 선택하셨습니다.");
+        //         await i.update({
+        //             embeds: [editBtn],
+        //             components: [],
+        //         });
+        //     }
 
-        collector.on("collect", async (i) => {
-            console.log(i);
-            if (i.customId === "join") {
-                const editBtn = new MessageEmbed()
-                    .setColor("#3498DB")
-                    .setDescription("참가를 선택하셨습니다.");
-                await i.update({
-                    embeds: [editBtn],
-                    ephemeral: true,
-                    components: [],
-                });
-            }
+        //     if (i.customId === "not join") {
+        //         const editBtn = new MessageEmbed()
+        //             .setColor("#3498DB")
+        //             .setDescription("참가안함을 선택하셨습니다.");
+        //         await i.update({
+        //             embeds: [editBtn],
+        //             components: [],
+        //         });
+        //     }
+        // });
 
-            if (i.customId === "not join") {
-                const editBtn = new MessageEmbed()
-                    .setColor("#3498DB")
-                    .setDescription("참가안함을 선택하셨습니다.");
-                await i.update({
-                    embeds: [editBtn],
-                    ephemeral: true,
-                    components: [],
-                });
-            }
-        });
-
-        collector.on("end", async (btnInterection) => {
-            btnInterection.forEach((click) => {
-                console.log(click.user.id, click.customId);
-            });
-            if (btnInterection.first.customId === "join") {
-            }
-        });
+        // collector.on("end", async (btnInterection) => {
+        //     btnInterection.forEach((click) => {
+        //         console.log(click.user.id, click.customId);
+        //     });
+        //     if (btnInterection.first.customId === "join") {
+        //     }
+        // });
     } else if (commandName === "마감") {
         const embed = new MessageEmbed()
             .setColor("#3498DB")
@@ -89,23 +83,20 @@ client.on("interactionCreate", async (interection) => {
             .setDescription(`1️⃣팀 :${userList}  \n 2️⃣팀 : test`)
             .setTimestamp();
         await interaction.reply({
-            ephemeral: true,
             embeds: [embed],
         });
         await interaction.reply(`${userList}`);
     }
 });
 
-// client.on("interactionCreate", (interaction) => {
-//     if (!interaction.isButton()) return;
-//     if (interaction.message.interaction.user.id !== interaction.user.id) {
-//         console.log(interaction);
-//     }
-//     // if (interaction.customId === "join") {
-//     //     userList.push(interaction.user.username);
-//     // }
-//     console.log(userList);
-// });
+client.on("interactionCreate", (interaction) => {
+    if (!interaction.isButton()) return;
+    if (interaction.customId === "join") {
+        interaction.reply({ content: "선택하셨습니다.", ephemeral: true });
+        userList.push(interaction.user.username);
+    }
+    console.log(userList);
+});
 
 client.login(process.env.TOKEN);
 
