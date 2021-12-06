79796c1b0fe8578939302098149008a805900f65
wold21 <wold2180@gmail.com>
2021-12-06 21:41:08 +0900

    - added args - added reset func

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,22 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
-const { token, guild, client } = require('./.config.json');
+const { token, guild, client } = require("./.config.json");
 
 const guildId = guild;
 const clientId = client;
 const commands = [
-    new SlashCommandBuilder().setName("team").addStringOption((option) => option.setName("a").setDescription("숫자만 입력가능합니다.").setRequired(true)).addStringOption((option) => option.setName("b").setDescription("숫자만 입력가능합니다.").setRequired(true)).setDescription("팀 만들기"),
+    new SlashCommandBuilder()
+        .setName("team")
+        .addStringOption((option) =>
+            option.setName("a").setDescription("숫자만 입력가능합니다.").setRequired(true)
+        )
+        .addStringOption((option) =>
+            option.setName("b").setDescription("숫자만 입력가능합니다.").setRequired(true)
+        )
+        .setDescription("팀 만들기"),
     new SlashCommandBuilder().setName("split").setDescription("모집 마감"),
+    new SlashCommandBuilder().setName("reset").setDescription("멤버 리셋"),
 ].map((command) => command.toJSON());
 
 const rest = new REST({ version: "9" }).setToken(token);
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {
     MessageButton,
     MessageEmbed,
 } = require("discord.js");
-const { token } = require('./.config.json');
+const { token } = require("./.config.json");
 
 const client = new Client({ intents: [32767] });
 
@@ -16,35 +16,31 @@ client.once("ready", () => {
 
 function memberProcess(arr, s1, s2) {
     let data = {};
-    if (arr.length < (s1 + s2)) {
+    let t1 = [];
+    let t2 = [];
+    let waitingMember = [];
+    let total = s1 + s2;
+
+    if (arr.length < total || arr.length === 0) {
         data.flag = false;
         data.length = arr.length;
     } else {
-        const t1 = new Array(s1);
-        const t2 = new Array(s2);
-
-        // 여기서 부터 작성 필요 =====================================
-        let waitingMember = [];
-        let team_one = [];
-        let team_two = [];
         arr.sort(() => Math.random() - 0.5);
-        arr.forEach((item, index) => {
-            if (index > 21) {
-                waitingMember.push(" " + item);
-            } else {
-                if (index % 2 != 0) {
-                    team_one.push(" " + item);
-                } else if (index % 2 == 0) {
-                    team_two.push(" " + item);
-                }
-            }
-        });
-        data.team_one = Array.from(new Set(team_one));
-        data.team_two = Array.from(new Set(team_two));
+        for (let i = 0; i < s1; i++) {
+            t1.push(" " + arr.pop());
+        }
+        for (let i = 0; i < s2; i++) {
+            t2.push(" " + arr.pop());
+        }
+
+        if (arr.length != 0) {
+            waitingMember.push(arr);
+        }
+        data.flag = true;
+        data.t1 = Array.from(new Set(t1));
+        data.t2 = Array.from(new Set(t2));
         data.waitingMember =
-            waitingMember.length != 0
-                ? Array.from(new Set(waitingMember))
-                : "대기 인원이 없습니다.";
+            arr.length != 0 ? Array.from(new Set(arr)) : "대기 인원이 없습니다.";
     }
     return data;
 }
@@ -70,41 +66,57 @@ let userList = [
     "eight",
 ];
 
+let memberCount = {};
 client.on("interactionCreate", async (interaction) => {
     const { commandName } = interaction;
     if (!interaction.isCommand()) return;
-    console.log(interaction.options.getString("a"));
-    console.log(interaction.options.getString("b"));
     if (commandName === "team") {
+        memberCount.a = parseInt(interaction.options.getString("a"));
+        memberCount.b = parseInt(interaction.options.getString("b"));
         const btn = new MessageActionRow().addComponents(
             new MessageButton().setCustomId("join").setLabel("참가").setStyle("SUCCESS"),
             new MessageButton().setCustomId("not").setLabel("참가안함").setStyle("DANGER")
         );
-        const embed = new MessageEmbed().setColor("#3498DB").setTitle("🛎 내전 모집");
+        const embed = new MessageEmbed().setColor("#FFFFFF").setTitle("🛎 내전 모집");
         await interaction.reply({
             embeds: [embed],
             components: [btn],
         });
     } else if (commandName === "split") {
-        let result = memberProcess(userList, a, b);
-        if (!result.flag) {
-            const embed = new MessageEmbed()
-                .setColor("#3498DB")
-                .setTitle("⚽️ 인원이 부족합니다")
-                .setDescription(`현재 신청 인원 ${result.length}명`);
+        let s1 = memberCount.a;
+        let s2 = memberCount.b;
+        let result = memberProcess(userList, s1, s2);
 
+        let embed;
+        if (!result.flag) {
+            if (result.length === 0) {
+                embed = new MessageEmbed()
+                    .setColor("#FFFFFF")
+                    .setTitle("⚽️ 인원을 모집해주세요");
+            } else {
+                embed = new MessageEmbed()
+                    .setColor("#FFFFFF")
+                    .setTitle("⚽️ 인원이 부족합니다")
+                    .setDescription(`현재 신청 인원 ${result.length}명`);
+            }
             await interaction.deferReply();
             await interaction.editReply({ embeds: [embed], components: [] });
         } else {
             const embed = new MessageEmbed()
-                .setColor("#3498DB")
+                .setColor("#FFFFFF")
                 .setTitle("⚽️ 결과")
                 .setDescription(
-                    `1️⃣팀 : ${result.team_one}  \n\n 2️⃣팀 : ${result.team_two} \n\n 📌 대기 : ${result.waitingMember}`
+                    `1️⃣팀 : ${result.t1}  \n\n 2️⃣팀 : ${result.t2} \n\n 📌 대기 : ${result.waitingMember}`
                 );
             await interaction.deferReply();
             await interaction.editReply({ embeds: [embed], components: [] });
         }
+    } else if (commandName === "reset") {
+        userList = [];
+        const embed = new MessageEmbed()
+            .setColor("#FFFFFF")
+            .setTitle("⚽️ 인원이 리셋됩니다.");
+        await interaction.reply({ embeds: [embed], components: [] });
     }
 });
 
@@ -122,7 +134,6 @@ client.on("interactionCreate", (interaction) => {
             "내전에 참여하지 않습니다.\n참가 신청을 했던 경우 팀 리스트에서 삭제됩니다.";
     }
     interaction.reply({ content: `${returnMsg}`, ephemeral: true });
-    console.log(userList);
 });
 
 client.login(token);
